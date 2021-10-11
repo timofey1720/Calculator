1ad54b22b65bda9543aa8b97f35e98be4d48f288
timofey1720 <borshchev.tima@bk.ru>
2021-10-11 21:55:30 +0300

    Commit 2

--- src/Utils.java
+++ src/Utils.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Utils {
     public static String getData(){
@@ -7,15 +8,29 @@ public class Utils {
         return string;
     }
     public static String[] parseParams(String string) throws Exception {
-        String vol = string.trim();
-        String[] ms = vol.split(" ");
-        if(ms.length <= 1) {
-            throw new Exception("строка не является математической операцией");
-        } else if(ms.length > 3) {
-            throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        } else {
-            return ms;
+
+        String[] str;
+        String[] rez = new String[3];
+        int indexpl = string.indexOf("+");
+        int indexmn = string.indexOf("-");
+        int indexpr = string.indexOf("*");
+        int indexdr = string.indexOf("/");
+        if(indexpl != -1){
+            return prsString(string, "+");
         }
+        else if(indexmn != -1){
+            return prsString(string, "-");
+        }
+        else if(indexpr != -1){
+            return prsString(string, "*");
+        }
+        else if(indexdr != -1){
+            return prsString(string, "/");
+        }
+        else {
+            throw new Exception("строка не является математической операцией");
+        }
+
     }
     public static int[] getCorrectSymbol(String pr) throws Exception {
         Integer romVl = getRomSymbol(pr);
@@ -58,5 +73,18 @@ public class Utils {
             }
         }
         return null;
+    }
+
+    private static String[] prsString(String string, String symb) throws Exception {
+
+        String[] rez = new String[3];
+        String[] str = string.split(Pattern.quote(symb),2);
+        rez[0] = str[0].trim();
+        if(str[1].split("[(/)|(-)|(+)|(*)]").length > 1) {
+            throw new Exception("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+        rez[2] = str[1].trim();
+        rez[1] = symb;
+        return rez;
     }
 }
